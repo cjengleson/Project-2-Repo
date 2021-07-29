b5cc098ea0a7c718050a97024bf1d43e40f4bec8
Cheyenne M <76500260+martiche93@users.noreply.github.com>
2021-07-28 21:53:44 -0700

    updated graphs.html/.css code

--- static/js/test-graphs.js
+++ static/js/test-graphs.js
@@ -17,6 +17,11 @@ var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{
 // Initialize all of the LayerGroups we'll be using
 var layers = {
   LOCATIONS: new L.LayerGroup(),
+  ACRES: new L.LayerGroup(),
+  YEARS: new L.LayerGroup(),
+  CAUSE: new L.LayerGroup(),
+  COUNTY: new L.LayerGroup(),
+  REPORT: new L.LayerGroup()
 };
 
 // Create the map with our layers
@@ -25,6 +30,11 @@ var map = L.map("heatmap", {
   zoom: 12,
   layers: [
     layers.LOCATIONS,
+    layers.ACRES,
+    layers.YEARS,
+    layers.CAUSE,
+    layers.COUNTY,
+    layers.REPORT
   ]
 });
 
@@ -34,6 +44,11 @@ lightmap.addTo(map);
 // Create an overlays object to add to the layer control
 var overlays = {
   "Locations": layers.LOCATIONS,
+  "Acres": layers.ACRES,
+  "Years": layers.YEARS,
+  "Cause": layers.CAUSE,
+  "County": layers.COUNTY,
+  "Report": layers.REPORT
 };
 
 // Create a control for our layers, add our overlay layers to it
@@ -59,67 +74,109 @@ var icons = {
     iconColor: "white",
     markerColor: "yellow",
     shape: "star"
+  }),
+  ACRES: L.ExtraMarkers.icon({
+      icon: "ion-android-bicycle",
+      iconColor: "white",
+      markerColor: "red",
+      shape: "circle"
+  }),
+  YEARS: L.ExtraMarkers.icon({
+      icon: "ion-minus-circled",
+      iconColor: "white",
+      markerColor: "blue-dark",
+      shape: "penta"
+  }),
+  CAUSE: L.ExtraMarkers.icon({
+      icon: "ion-android-bicycle",
+      iconColor: "white",
+      markerColor: "orange",
+      shape: "circle"
+  }),
+  COUNTY: L.ExtraMarkers.icon({
+      icon: "ion-android-bicycle",
+      iconColor: "white",
+      markerColor: "green",
+      shape: "circle"
+  }),
+  REPORT: L.ExtraMarkers.icon({
+      icon: "ion-android-bicycle",
+      iconColor: "white",
+      markerColor: "black",
+      shape: "square"
   })
 };
 
 // Perform an API call to the cleaned Historical Wildfires in OR
 d3.json("../Resources/or_df.csv").then(function(wildfires) {
+    var updatedAt = wildfires.last_updated;
+    var response = wildfires.data.locations;
 
-    // Create an object to keep the number of markers in each layer
+    // Create an object to keep the number of markers in locations
     var locationsCount = {
       LOCATIONS: 0,
+      ACRES: 0,
+      YEARS: 0,
+      CAUSE: 0,
+      COUNTY: 0,
+      REPORT: 0
     };
 
-    // Initialize a locationsRadius, which will be used as a key to access the appropriate layers, icons, and radius count for layer group
-    var locationsRadius;
-
-        // Create a new location object with properties of both location objects
-        var location = Object.assign({}, )
+    // Initialize a locationsRadiusCode, which will be used as a key to access the appropriate layers, icons, and radius count for layer group
+    var locationsRadiusCode;
 
     // loop through response (i.e., the csv) and push each element's information into the respective array
-        for (var i = 0; i < response.length; i++) {
-        var lat = response[i].latitude;
-        var lon = response[i].longitude;
-        var acres = response[i].total_acres;
-        var year = response[i].fire_year;
-        var cause = response[i].general_cause;
-        var county = response[i].county;
-        var report_date = response[i].report_date;
-        if (lat) {
-        locations.push([lat, lon]);
-        acresArray.push(acres);
-        yearsArray.push(year);
-        causeArray.push(cause);
-        countyArray.push(county);
-        reportArray.push(report_date);
+    for (var i = 0; i < response.length; i++) {
+
+        // Create a new location object with properties of both location objects
+        var location = Object.assign({}, response[i]);
+        // If a location 
+        if (!location.locationsCount) {
+            locationsRadiusCode = "LOCATIONS";
+        }
+        else if (!location.acres) {
+            locationsRadiusCode = "ACRES";
+        }
+        else if (!location.fire_year) {
+            locationsRadiusCode = "YEARS";
+        }
+        else if (!locations.general_cause) {
+            locationsRadiusCode = "CAUSE";
+        }
+        else if (!locations.county) {
+            locationsRadiusCode = "COUNTY";
+        }
+        else (!locations.report_date) {
+            locationsRadiusCode = "REPORT";
+        };
+
+        // Update the location count
+        locationsCount[locationsRadiusCode]++;
+        // Create a new marker with the appropriate icon and coordinates
+        var newMarker = L.marker([location.lat, location.lon], {
+            icon: icons[locationsRadiusCode]
+        });
+
+        // Add the new marker to the appropriate layer
+        newMarker.addTo(layers[locationsRadiusCode]);
+
+        // Bind a popup to the marker that will display on click. This will be rendered as HTML
+        newMarker.bindPopup(location.name + "<br> County: " + location.county + "<br> Acres: " + location.acres + "<br> Year: " + location.year + "Wildfires!");
         }
 
-      // Create a new marker with the appropriate icon and coordinates
-      var newMarker = L.marker([station.lat, station.lon], {
-        icon: icons[stationStatusCode]
-      });
-
-      // Add the new marker to the appropriate layer
-      newMarker.addTo(layers[stationStatusCode]);
-
-      // Bind a popup to the marker that will  display on click. This will be rendered as HTML
-      newMarker.bindPopup(station.name + "<br> Capacity: " + station.capacity + "<br>" + station.num_bikes_available + " Bikes Available");
-    }
-
-    // Call the updateLegend function, which will... update the legend!
-    updateLegend(updatedAt, stationCount);
-  });
+        // Call the updateLegend function, which will... update the legend!
+        updateLegend(updatedAt, locationsCount);
+    });
 });
 
-// Update the legend's innerHTML with the last updated time and station count
-function updateLegend(time, stationCount) {
-  document.querySelector(".legend").innerHTML = [
-    "<p>Updated: " + moment.unix(time).format("h:mm:ss A") + "</p>",
-    "<p class='out-of-order'>Out of Order Stations: " + stationCount.OUT_OF_ORDER + "</p>",
-    "<p class='coming-soon'>Stations Coming Soon: " + stationCount.COMING_SOON + "</p>",
-    "<p class='empty'>Empty Stations: " + stationCount.EMPTY + "</p>",
-    "<p class='low'>Low Stations: " + stationCount.LOW + "</p>",
-    "<p class='healthy'>Healthy Stations: " + stationCount.NORMAL + "</p>"
-  ].join("");
-}
-});
+// Update the legend's innerHTML with the last updated time and location count
+function updateLegend(time, locationsCount) {
+    document.querySelector(".legend").innerHTML = [
+        "<p>Updated: " + moment.unix(time).format("h:mm:ss A") + "</p>",
+        "<p class='acres'>Acres Burned: " + location.ACRES + "</p>",
+        "<p class='years'>Years: " + location.YEARS + "</p>",
+        "<p class='cause'>Cause: " + location.CAUSE + "</p>",
+        "<p class='county'>County: " + location.COUNTY + "</p>",
+        "<p class='report'>Report: " + location.REPORT + "</p>"
+    ].join("");
+};
